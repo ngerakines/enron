4cb1d37fed21eb6dc07077e2e82fae246590523b
Nick Gerakines <nick@gerakines.net>
2013-04-20 13:01:49 -0700

    initial commit

---
+++ src/main/java/com/socklabs/enron/EmailTree.java
@@ -0,0 +1,11 @@
+package com.socklabs.enron;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ngerakines
+ * Date: 4/20/13
+ * Time: 1:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmailTree {
+}
